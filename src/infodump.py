581f5bacafc7baef1695720792965d558a37def2
Drew Wheeler <wheelercs@protonmail.com>
2023-03-26 11:05:31 -0500

    Export AnalysisEngine, PE, and COFF Header
    Can export the Analysis Engine's analysis, the information relating to the portable executable, and (most of) the COFF Header. Still have to work around how to deal with the `characteristics` attribute.

--- src/infodump.py
+++ src/infodump.py
@@ -32,25 +32,59 @@ class InfoDump (object):
         self.__analyzer.validate_sizes ()
         self.__analyzer.check_section_names ()
 
-    def export_all (self, output_file:str = "out.json") -> None:
+    def export_and_write (self, output_file:str = "out.json") -> None:
         with open (output_file, 'w') as exporter:
-            self.export_analysis_engine (export_stream = exporter)
-
-    def export_analysis_engine (self, export_stream = None) -> None:
-        # Only export if the stream passed in is active
-        if export_stream is not None:
-            # Create a temporary dictionary to allow for json.dump to work
-            out_dict = {
+            # Generate dictionaries containing component data
+            a_eng =self.export_analysis_engine ()
+            pe = self.export_portable_executable ()
+            ch = self.export_coff_header ()
+            # Aggregate data and prepare to be written to JSON
+            export_dict = {
                 "file" : self.__analyzer.input_file,
-                "mismatched sizes" : self.__analyzer.flag_mismatched_sizes,
-                "is packed" : self.__analyzer.flag_packed_sections,
-                "packing type" : self.__analyzer.packing_type,
-                "packed sections" : self.__analyzer.packed_section_count,
-                "vt called" : self.__api_checks,
-                "vt match" : self.__analyzer.flag_vt_match,
-                "vt match ratio" : self.__analyzer.vt_match_ratio
+                "analysis" : a_eng,
+                "overview" : pe,
+                "coff" : ch
             }
-            json.dump (out_dict, export_stream)
+            json.dump (export_dict, exporter, indent = 4)
+
+    def export_analysis_engine (self) -> dict():
+        out_dict = {
+            "mismatched sizes" : self.__analyzer.flag_mismatched_sizes,
+            "is packed" : self.__analyzer.flag_packed_sections,
+            "packing type" : self.__analyzer.packing_type,
+            "packed sections" : self.__analyzer.packed_section_count,
+            "vt called" : self.__api_checks,
+            "vt match" : self.__analyzer.flag_vt_match,
+            "vt match ratio" : self.__analyzer.vt_match_ratio
+        }
+        return out_dict
+    
+    def export_coff_header (self) -> dict():
+        out_dict = {
+            "target machine" : str(self.__analyzer.executable.coff_header.target_machine),
+            "section quantity" : self.__analyzer.executable.coff_header.section_quantity,
+            "timestamp" : self.__analyzer.executable.coff_header.timestamp,
+            "sym table ptr" : self.__analyzer.executable.coff_header.sym_table_ptr,
+            "sym quantity" : self.__analyzer.executable.coff_header.symbol_quantity,
+            "opt header size" : self.__analyzer.executable.coff_header.opt_header_size
+            #"characteristics" : list (self.__analyzer.executable.coff_header.characteristics)
+        }
+        return out_dict
+
+    def export_portable_executable (self) -> dict():
+        out_dict = {
+            "size" : self.__analyzer.executable.size,
+            "virtual size" : self.__analyzer.executable.virtual_size,
+            "md5" : self.__analyzer.executable.md5,
+            "sha1" : self.__analyzer.executable.sha1,
+            "sha256" : self.__analyzer.executable.sha256,
+            "e_lfanew" : self.__analyzer.executable.e_lfanew,
+            "min entropy" : self.__analyzer.executable.sec_min_entropy,
+            "avg entropy" : self.__analyzer.executable.sec_avg_entropy,
+            "max entropy" : self.__analyzer.executable.sec_max_entropy,
+            "has load cfg" : self.__analyzer.executable.has_cfg
+        }
+        return out_dict
 
     
     # Accessor and Mutators
@@ -75,4 +109,4 @@ class InfoDump (object):
 
 if __name__ == "__main__":
     export_data = InfoDump (target_file = argv[1], do_api_checks = False)
-    export_data.export_all (output_file = "analysis.json")
+    export_data.export_and_write (output_file = "analysis.json")
